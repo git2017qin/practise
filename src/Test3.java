053371e458d5108b3e6c61abeb6d8d823cb7efb8
lingqin <uestc_qin@126.com>
2017-07-13 09:39:57 +0800

    输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。

--- src/Test3.java
+++ src/Test3.java
@@ -5,22 +5,12 @@ import java.util.ArrayList;
  */
 public class Test3 {
     public static void main(String[] args) {
-//        TreeNode t1 = new TreeNode(1);
-//        TreeNode t2 = new TreeNode(2);
-//        TreeNode t3 = new TreeNode(3);
-//        TreeNode t4 = new TreeNode(4);
-//        TreeNode t5 = new TreeNode(5);
-//        TreeNode t6 = new TreeNode(6);
-//        TreeNode t7 = new TreeNode(7);
-//        t1.left = t2;
-//        t1.right = t5;
-//        t2.left = t3;
-//        t2.right = t4;
-//        t5.left = t6;
-//        t5.right = t7;
+
         int[] pre = {1,2,3,4,5,6,7};
         int[] in = {3,2,4,1,6,5,7};
+
         TreeNode t = reConstructBinaryTree(pre,in);
+
         System.out.println(t.val);
         System.out.println(t.left.val);
         System.out.println(t.right.val);
